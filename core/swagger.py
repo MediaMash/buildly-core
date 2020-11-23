b13484b5694ec3caf2fd1130812ffd9be557df36
vishalajackus <vishal.hande@ajackus.com>
2020-11-23 18:51:31 +0530

    change uuid type from number to string

--- core/swagger.py
+++ core/swagger.py
@@ -47,8 +47,8 @@ COREUSER_INVITE_EVENT_CHECK_RESPONSE = {200: Schema(
     type='object',
     properties={
         'email': Schema(type='string'),
-        'room_uuid': Schema(type='number'),
-        'event_uuid': Schema(type='number'),
+        'room_uuid': Schema(type='string'),
+        'event_uuid': Schema(type='string'),
         'organization': Schema(type='string'),
     })
 
